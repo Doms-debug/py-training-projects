59b5ebd8257be47bb46dbdc0b5ea8d89c131248d
Doms-debug <dominik.czajka@onet.pl>
2026-07-22 17:22:08 +0200

    adjusting arguments

--- 01_basic/03_password_generator/generator.py
+++ 01_basic/03_password_generator/generator.py
@@ -2,14 +2,15 @@ import argparse
 import string
 import random
 
-all_signs = string.printable
+all_letters = string.ascii_letters
+
 parser = argparse.ArgumentParser(
     prog='Pass-gen v3',
     description='Generate secure passwords using python script. Declare desired number of signs.'
 )
 
 def main():
-    pass_length = int(input("Pass a number for your required password length: "))
+    parser.add_argument('length', type=int, help='Password length')
     parser.add_argument('--use-symbols', action='store_true')
     parser.add_argument('--use-numbers', action='store_true')
 
