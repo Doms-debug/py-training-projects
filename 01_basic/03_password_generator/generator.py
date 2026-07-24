a19faf4bdea20aa8e9b85fd227675a0e5f7268af
Doms-debug <dominik.czajka@onet.pl>
2026-07-24 13:05:44 +0200

    finish pass gen project

--- 01_basic/03_password_generator/generator.py
+++ 01_basic/03_password_generator/generator.py
@@ -14,9 +14,28 @@ parser = argparse.ArgumentParser(
 
 def main():
     parser.add_argument('length', type=int, help='Password length')
-    parser.add_argument('--use-symbols', action='store_true')
-    parser.add_argument('--use-numbers', action='store_true')
+    parser.add_argument('--use-symbols', action='store_true', help='Include punctuation marks')
+    parser.add_argument('--use-numbers', action='store_true', help='Include digits')
 
     args = parser.parse_args()
 
-main()
+    # start with the base pool of letters
+    character_pool = all_letters
+
+    # dynamically expand the pool based on user flags
+    if args.use_numbers:
+        character_pool += string.digits
+        
+    if args.use_symbols:
+        character_pool += string.punctuation
+
+    # random.choices picks 'k' elements from the pool (allows duplicates)
+    generated_chars = random.choices(character_pool, k=args.length)
+
+    # join the list elements into one string
+    final_password = "".join(generated_chars)
+
+    print(final_password)
+
+if __name__ == '__main__':
+    main()
