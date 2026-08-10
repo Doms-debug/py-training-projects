5733946bcb8cc4cecaf91c9dd4707154c9bfa018
Doms-debug <dominik.czajka@onet.pl>
2026-08-10 17:59:55 +0200

    fixing bugs

--- 01_basic/06_todo_list/main.py
+++ 01_basic/06_todo_list/main.py
@@ -4,18 +4,18 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
  
-    if 'add' in user_action:
+    if user_action.startswith('add'):
         todo = user_action[4:]
  
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
  
-        todos.append(todo)
+        todos.append(todo + '\n')
  
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
  
-    elif 'show' in user_action:
+    elif user_action.startswith('show'):
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
  
@@ -24,7 +24,7 @@ while True:
             row = f"{index + 1}.{item} "
             print(row)
 
-    elif 'edit' in user_action:
+    elif user_action.startswith('edit'):
         number = int(user_action[5:])
         print(number)
  
@@ -39,7 +39,7 @@ while True:
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
  
-    elif 'complete' in user_action:
+    elif user_action.startswith('complete'):
         number = int(user_action[9:])
  
         with open('todos.txt', 'r') as file:
@@ -54,7 +54,7 @@ while True:
         message = f"Todo {todo_to_remove} was removed from the list."
         print(message)
  
-    elif 'exit' in user_action:
+    elif user_action.startswith('exit'):
         break
     else:
         print("Command is not valid.")
