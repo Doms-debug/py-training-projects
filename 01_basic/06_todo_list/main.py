e2e15e7bfb6b77e045ec4288615e1f76aba6c6a1
Doms-debug <dominik.czajka@onet.pl>
2026-08-10 18:05:29 +0200

    Error handling

--- 01_basic/06_todo_list/main.py
+++ 01_basic/06_todo_list/main.py
@@ -25,35 +25,46 @@ while True:
             print(row)
 
     elif user_action.startswith('edit'):
-        number = int(user_action[5:])
-        print(number)
- 
-        number = number - 1
- 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
- 
-        new_todo = input("Enter new todo: ")
-        todos[number] = new_todo + '\n'
- 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        try:
+            number = int(user_action[5:])
+            print(number)
+    
+            number = number - 1
+    
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+    
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + '\n'
+    
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+        except ValueError:
+            print("Your command is not valid")
+            continue
+
  
     elif user_action.startswith('complete'):
-        number = int(user_action[9:])
- 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
-        index = number - 1
-        todo_to_remove = todos[index].strip('\n')
-        todos.pop(index)
- 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
- 
-        message = f"Todo {todo_to_remove} was removed from the list."
-        print(message)
- 
+        try:
+            number = int(user_action[9:])
+    
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+    
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+    
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
+
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+
     elif user_action.startswith('exit'):
         break
     else:
