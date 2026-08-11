942fa40d40fc3fd58593635915f36ffae7822973
Doms-debug <dominik.czajka@onet.pl>
2026-08-11 16:48:34 +0200

    custom functions for DRY code

--- 01_basic/06_todo_list/main.py
+++ 01_basic/06_todo_list/main.py
@@ -1,5 +1,10 @@
 todos = []
 
+def get_todos():
+    with open('todos.txt', 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
@@ -7,8 +12,7 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
  
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
  
         todos.append(todo + '\n')
  
@@ -16,8 +20,7 @@ while True:
             file.writelines(todos)
  
     elif user_action.startswith('show'):
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
  
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -31,8 +34,7 @@ while True:
     
             number = number - 1
     
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
     
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
@@ -49,8 +51,8 @@ while True:
         try:
             number = int(user_action[9:])
     
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
+
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
@@ -63,10 +65,10 @@ while True:
 
         except IndexError:
             print("There is no item with that number.")
-            continue
 
     elif user_action.startswith('exit'):
         break
+
     else:
         print("Command is not valid.")
  
