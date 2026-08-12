1c9d8588a2dd516be495df7c1b04d1d74eda0c17
Doms-debug <dominik.czajka@onet.pl>
2026-08-12 14:02:08 +0200

    optimizing code with redundant funcs

--- 01_basic/06_todo_list/main.py
+++ 01_basic/06_todo_list/main.py
@@ -1,9 +1,13 @@
-todos = []
-
-def get_todos():
-    with open('todos.txt', 'r') as file_local:
+def get_todos(filepath):
+    with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
-    return todos
+    return todos_local
+
+
+def write_todos(filepath, todos_local):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_local)
+
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -12,15 +16,14 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
  
-        todos = get_todos()
+        todos = get_todos("todos.txt")
  
         todos.append(todo + '\n')
  
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos("todos.txt", todos)
  
     elif user_action.startswith('show'):
-        todos = get_todos()
+        todos = get_todos("todos.txt")
  
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -34,31 +37,28 @@ while True:
     
             number = number - 1
     
-            todos = get_todos()
+            todos = get_todos("todos.txt")
     
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
     
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
 
         except ValueError:
             print("Your command is not valid")
             continue
-
  
     elif user_action.startswith('complete'):
         try:
             number = int(user_action[9:])
     
-            todos = get_todos()
+            todos = get_todos("todos.txt")
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
     
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
     
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
